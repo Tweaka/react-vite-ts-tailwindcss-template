136c9568291c5d4c414038e22f0d4c6ce22680c8
Tweaka <vzxpk@yahoo.fr>
2021-12-22 16:11:33 +0100

    handled home route and 404

--- src/App.tsx
+++ src/App.tsx
@@ -4,28 +4,22 @@ const App: React.FC = () => {
   return (
     <div className="app">
       <header className="app-header text-center">
-        <h1 className="bg-blue-600 text-5xl border-neutral-800 font-extrabold m-5">template</h1>
+        <h1 className="bg-blue-600 text-5xl border-neutral-800 font-extrabold m-5"> vite + typescript + tailwindcss Template</h1>
         <p className="callout">
           Edit <code>/src/routes/index.ts</code> for routes config.
         </p>
         <p>
           <a
             className="app-link"
-            href="https://reactjs.org"
+            href="https://daisyui.com/"
             target="_blank"
             rel="noopener noreferrer"
           >
-            <code className="underline text-green-600 ">template</code>
+            <p className="underline text-green-600 ">add daisyui</p>
           </a>
-          {' | '}
-          <a
-            className="app-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            <code className="underline text-green-600">welcome</code>
-          </a>
+          <code>
+            npm i daisyui <br /> require('daisyui'), => tailwindconfig/plugins
+          </code>
         </p>
       </header>
     </div>
--- src/views/ErrorPage.tsx
+++ src/views/ErrorPage.tsx
@@ -1,7 +1,14 @@
 import * as React from 'react';
 
 const ErrorPage: React.FC = () => {
-  return <h1>404 Not Found</h1>;
+  return (
+    <div className="bg-red-800">
+      <a className="underline absolute pt-2 pl-2 " href="/">
+        back to home page
+      </a>
+      <img className="w-full" src="src/images/bison.jpg" alt="404 not found" />
+    </div>
+  );
 };
 
 export default ErrorPage;
